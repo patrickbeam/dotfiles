42e13c8c925f1142b25228cfda4b99040436b76c
Patrick Beam <patrickbeam@beamer-mbp.local>
2023-01-15 12:53:17 -0600

    checking in neovim lua config

---
+++ .config/nvim/lua/patrick/remap.lua
@@ -0,0 +1,13 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.cmd("cnoreabbrev W! w!")
+vim.cmd("cnoreabbrev Q! q!")
+vim.cmd("cnoreabbrev Qall! qall!")
+vim.cmd("cnoreabbrev Wq wq")
+vim.cmd("cnoreabbrev Wa wa")
+vim.cmd("cnoreabbrev wQ wq")
+vim.cmd("cnoreabbrev WQ wq")
+vim.cmd("cnoreabbrev W w")
+vim.cmd("cnoreabbrev Q q")
+vim.cmd("cnoreabbrev Qall qall")
